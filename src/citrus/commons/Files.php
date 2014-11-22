6d52a5a27c5a000c693ab69e5249c980437a35c4
nullpunkt <tobias.neeb@gmail.com>
2014-11-22 08:27:50 +0100

    added deleteDir to Files

--- src/citrus/commons/Files.php
+++ src/citrus/commons/Files.php
@@ -91,4 +91,25 @@ class Files {
 		}
 		return true;
 	}
+
+    /**
+     * Deletes $dir and all files directories within
+     * @param $dir
+     */
+    public function deleteDir($dir) {
+        $it = new RecursiveDirectoryIterator($dir, RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = new RecursiveIteratorIterator($it,
+            RecursiveIteratorIterator::CHILD_FIRST);
+        foreach($files as $file) {
+            if ($file->getFilename() === '.' || $file->getFilename() === '..') {
+                continue;
+            }
+            if ($file->isDir()){
+                rmdir($file->getRealPath());
+            } else {
+                unlink($file->getRealPath());
+            }
+        }
+        rmdir($dir);
+    }
 }
